06c2028b8e859f64148fe8e09ac28a353c952237
zyf <zhaoyafei0210@gmail.com>
2018-02-21 05:18:31 +0800

    add src/parse_log_zyf/parse_train_log.py and src/parse_log_zyf/plot_parse_results.py

---
+++ src/parse_log_zyf/parse_train_log.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 20 22:30:09 2018
+
+@author: zhaoy
+"""
+import os
+import sys
+import os.path as osp
+
+import re
+
+
+rlt_train_detail_basename = 'train_acc_detail.txt'
+rlt_train_basename = 'train_acc.txt'
+rlt_verif_basename = 'verif_acc.txt'
+
+
+def parse_train_log(log_fn, save_dir=None, save_train_detail=False):
+    if save_dir is None:
+        save_dir = './rlt_parse_log'
+
+    if not osp.exists(save_dir):
+        os.makedirs(save_dir)
+
+    fp = open(log_fn, 'r')
+
+    fp_out_train_detail = None
+    if save_train_detail:
+        fn_out_train_detail = osp.join(save_dir, rlt_train_detail_basename)
+        fp_out_train_detail = open(fn_out_train_detail, 'w')
+        write_string = 'epoch\tbatch\tspeed(samples/sec)\tacc\n'
+        fp_out_train_detail.write(write_string)
+
+    fn_out_train = osp.join(save_dir, rlt_train_basename)
+    fn_out_verif = osp.join(save_dir, rlt_verif_basename)
+
+    fp_out_train = open(fn_out_train, 'w')
+    fp_out_verif = open(fn_out_verif, 'w')
+
+    write_string = 'epoch\ttrain-acc\ttime-cost\n'
+    fp_out_train.write(write_string)
+
+    write_string = 'batch\tacc_avg_verifDB'
+    write_string += '\tinfer_time(lfw)\txnorm(lfw)\tacc(lfw)\tacc_std(lfw)'
+    write_string += '\tinfer_time(cfp_ff)\txnorm(cfp_ff)\tacc(cfp_ff)\tacc_std(cfp_ff)'
+    write_string += '\tinfer_time(cfp_fp)\txnorm(cfp_fp)\tacc(cfp_fp)\tacc_std(cfp_fp)'
+    write_string += '\tinfer_time(age)\txnorm(age)\tacc(age)\tacc_std(age)\tAccuracy-Highest\n'
+    fp_out_verif.write(write_string)
+
+    train_write_string = ''
+    verif_write_string = ''
+
+    line_cnt = 0
+    train_epoch_idx = 0
+    test_batch_idx = 0
+
+    verif_acc_avg = 0.0
+    verif_db_cnt = 0
+
+    for line in fp:
+        line_cnt += 1
+
+        if line.startswith('INFO:root:Epoch'):
+            if save_train_detail and 'Batch' in line:
+                spl = re.split('[^0-9.]+', line)
+                write_string = '\t{:10}\t{:10}\t{:10}\t{:10}\n'.format(
+                    spl[1], spl[2], spl[3], spl[4]
+                )
+                fp_out_train_detail.write(write_string)
+
+            if 'Train-acc' in line:
+                line = line.strip()
+
+                train_epoch_idx = re.split('\D+', line)[1]
+                train_write_string += '{:10}\t{:10}'.format(
+                    train_epoch_idx, line.split('=')[-1])
+
+            if 'Time cost' in line:
+                line = line.strip()
+
+                train_write_string += '\t{:10}\n'.format(line.split('=')[-1])
+                fp_out_train.write(train_write_string)
+                train_write_string = ''
+
+        if line.startswith('infer time'):
+            verif_write_string += '\t{:10}'.format(line.split()[-1])
+
+        if 'XNorm:' in line:
+            verif_write_string += '\t{:10}'.format(line.split()[-1])
+            if line.startswith('[lfw]'):
+                test_batch_idx = re.split('\D+', line)[1]
+
+        if 'Accuracy-Flip:' in line:
+            _acc = line.split()[-1]
+            acc = _acc.split('+-')
+
+            verif_acc_avg += float(acc[0])
+            verif_db_cnt += 1
+
+            verif_write_string += '\t{:10}\t{:10}'.format(acc[0], acc[1])
+
+        if 'Accuracy-Highest:' in line:
+            verif_write_string += '\t{:10}\n'.format(line.split()[-1])
+            fp_out_verif.write('{:10}\t{:10}'.format(
+                test_batch_idx, verif_acc_avg / verif_db_cnt) + verif_write_string)
+
+            verif_write_string = ''
+            verif_acc_avg = 0.0
+            verif_db_cnt = 0
+
+        if line_cnt % 1000 == 0:
+            print('---> {} lines processed'.format(line_cnt))
+            fp_out_train.flush()
+            fp_out_verif.flush()
+
+            if save_train_detail:
+                fp_out_train_detail.flush()
+
+    fp_out_train.close()
+    fp_out_verif.close()
+    if save_train_detail:
+        fp_out_train_detail.close()
+
+
+if __name__ == '__main__':
+    log_fn = './train-log.txt'
+    save_dir = './rlt_parse_log'
+    save_train_detail = True
+
+    if len(sys.argv) > 1:
+        log_fn = sys.argv[1]
+
+    if len(sys.argv) > 2:
+        save_dir = sys.argv[2]
+
+    if len(sys.argv) > 3:
+        save_train_detail = True
+
+    parse_train_log(log_fn, save_dir, save_train_detail)
---
+++ src/parse_log_zyf/plot_parse_results.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 20 22:30:09 2018
+
+@author: zhaoy
+"""
+import os
+import sys
+import os.path as osp
+
+import re
+import numpy as np
+
+import matplotlib
+matplotlib.use('agg')
+
+from matplotlib import pyplot as plt
+
+
+rlt_train_detail_basename = 'train_acc_detail.txt'
+rlt_train_basename = 'train_acc.txt'
+rlt_verif_basename = 'verif_acc.txt'
+
+
+def load_train_results_and_plot(save_dir, train_rlt_fn=None, batchs_per_epoch=0):
+    if save_dir is None:
+        save_dir = './rlt_parse_log'
+
+    if not osp.exists(save_dir):
+        os.makedirs(save_dir)
+
+    if train_rlt_fn is None:
+        train_rlt_fn = osp.join(save_dir, rlt_train_basename)
+
+    data = np.loadtxt(train_rlt_fn, skiprows=1)
+    print('loaded data shape: ', data.shape)
+
+    def plot_data(use_batch_idx=False):
+        xx = data[:, 0]
+
+        if not use_batch_idx:
+            x_label = 'epoch'
+        else:
+            xx *= batchs_per_epoch
+            x_label = 'batch'
+
+        fig = plt.figure(figsize=(16, 12), dpi=100)
+
+        plt.plot(xx, data[:, 1], 'g')
+
+        # plt.xlim([0, 1e8])
+        plt.ylim([0, 1])
+
+        plt.xlabel(x_label)
+        plt.ylabel('train acc')
+
+        plt.grid()
+        plt.legend(loc='lower right')
+        plt.show()
+
+        save_fn = 'train_acc_vs_%s_idx.png' % x_label
+        save_fn = osp.join(save_dir, save_fn)
+
+        fig.savefig(save_fn, bbox_inches='tight')
+
+    plot_data()
+    if batchs_per_epoch > 0:
+        plot_data(use_batch_idx=True)
+
+
+def load_verif_results_and_plot(save_dir, verif_rlt_fn=None, batchs_per_epoch=0):
+    if save_dir is None:
+        save_dir = './rlt_parse_log'
+
+    if not osp.exists(save_dir):
+        os.makedirs(save_dir)
+
+    if verif_rlt_fn is None:
+        verif_rlt_fn = osp.join(save_dir, rlt_verif_basename)
+
+    data = np.loadtxt(verif_rlt_fn, skiprows=1)
+    print('loaded data shape: ', data.shape)
+
+    colors = ['g', 'r', 'b', 'c', 'm', 'y']
+    labels = ['avg_dbs', 'lfw', 'cfp_ff', 'cfp_fp', 'age_db', 'age_db_highest']
+
+    def plot_data(use_epoch_idx=False):
+        xx = data[:, 0]
+        if use_epoch_idx > 0:
+            xx /= batchs_per_epoch
+            x_label = 'epoch'
+        else:
+            x_label = 'batch'
+
+        fig = plt.figure(figsize=(16, 12), dpi=100)
+
+        plt.plot(xx, data[:, 1], colors[0], label=labels[0])
+
+        for i in range(1, 5):
+            plt.plot(xx, data[:, 4 * i], colors[i], label=labels[i])
+
+        plt.plot(xx, data[:, -1], colors[-1], label=labels[-1])
+
+        # plt.xlim([0, 1e8])
+        plt.ylim([0, 1])
+
+        plt.xlabel(x_label)
+        plt.ylabel('verif acc')
+
+        plt.grid()
+        plt.legend(loc='lower right')
+        plt.show()
+
+        save_fn = 'verif_acc_vs_%s_idx.png' % x_label
+
+        save_fn = osp.join(save_dir, save_fn)
+        fig.savefig(save_fn, bbox_inches='tight')
+
+    plot_data(use_epoch_idx=False)
+
+    if batchs_per_epoch > 0:
+        plot_data(use_epoch_idx=True)
+
+
+if __name__ == '__main__':
+    save_dir = './rlt_parse_log'
+    batchs_per_epoch = 7920
+
+    if len(sys.argv) > 1:
+        log_fn = sys.argv[1]
+
+    if len(sys.argv) > 2:
+        batchs_per_epoch = int(sys.argv[2])
+
+    load_verif_results_and_plot(save_dir, batchs_per_epoch=batchs_per_epoch)
+    load_train_results_and_plot(save_dir, batchs_per_epoch=batchs_per_epoch)
