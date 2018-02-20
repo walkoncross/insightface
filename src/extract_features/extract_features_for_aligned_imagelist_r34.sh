384a303b3b793c75258006e539da8f9b82cd523f
zyf <zhaoyafei0210@gmail.com>
2018-02-20 08:50:13 +0800

    edit paths in src/extract_features/extract_features_for_aligned_imagelist_r34.sh and src/extract_features/extract_features_for_aligned_imagelist_r50.sh

--- src/extract_features/extract_features_for_aligned_imagelist_r34.sh
+++ src/extract_features/extract_features_for_aligned_imagelist_r34.sh
@@ -2,16 +2,15 @@ export PYTHONPATH=/usr/local/lib/python2.7/dist-packages:$PYTHON
 export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
 
 nohup python extract_features_for_aligned_imagelist.py \
-    --model=C:\zyf\dnn_models\face_models\insight-face\model-r34-amf\model,0 \
-    --image-list=../../test_data/face_chips_list.txt \
-    --image-dir=../../test_data/face_chips \
-    --save-dir=./rlt-features-r34-amf \
-    --batch-size=2 \
+    --model=../../models/model-r34-amf/model,0 \
+    --image-list=/disk2/data/FACE/face-idcard-1M/face-idcard-1M-image-list.txt \
+    --image-dir=/disk2/data/FACE/face-idcard-1M/face-idcard-1M-mtcnn-aligned-112x112/aligned_imgs \
+    --save-dir=/disk2/data/FACE/face-idcard-1M/features/insightface-r34-amf \
+    --batch-size=256\
     --image-size=3,112,112 \
     --add-flip \
     --gpu=0 \
-    --save-format=.npy \
-    --flip-sim  \
-        > nohup-extract-log.txt &
+    --save-format=.bin \
+    --flip-sim > nohup-extract-log.txt &
 
 #    --use-mean \
